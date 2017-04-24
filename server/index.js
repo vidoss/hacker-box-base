b008bc4cfcccbcc56cc1f626a8687f09d4dc1a84
Victor Doss <victor.doss@gmail.com>
2017-04-24 15:55:14 -0700

    Add deps. cleanup unwanted files

--- server/index.js
+++ server/index.js
@@ -8,11 +8,9 @@ const IS_PRODUCTION = "production" === process.env.NODE_ENV;
 
 module.exports = configure(express()).then(server => {
 
-  if (IS_PRODUCTION) {
-    // On production, use the public directory for static files
-    // This directory is created by webpack on build time.
-    server.use(express.static(`${__dirname}/../dist`));
-  }
+  // Use the public directory for static files
+  // This directory is created by webpack on build time.
+  server.use(express.static(`${__dirname}/../dist`));
 
   // Mount api routes, errors are reported as JSON.
   server.use('/api', api, handleError);
